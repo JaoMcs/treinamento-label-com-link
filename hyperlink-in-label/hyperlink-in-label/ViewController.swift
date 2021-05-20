cc54f4c536b91af84b2329e426d7a6af65265868
João Marcos <joaort.azul@gmail.com>
2021-05-20 11:55:17 -0300

    Inicio do projeto

---
+++ hyperlink-in-label/hyperlink-in-label/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  hyperlink-in-label
+//
+//  Created by João Marcos on 20/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
