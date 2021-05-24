630a612d99b14e5601491f48ed0e3fa616a76b3e
João Marcos <joaort.azul@gmail.com>
2021-05-24 11:16:54 -0300

    Ajuste na indentação

--- hyperlink-in-label/hyperlink-in-label/ViewController.swift
+++ hyperlink-in-label/hyperlink-in-label/ViewController.swift
@@ -13,7 +13,6 @@ class ViewController: UIViewController {
   
   var urlString = [String : NSRange]()
   
-
   override func viewDidLoad() {
     super.viewDidLoad()
     
@@ -24,66 +23,66 @@ class ViewController: UIViewController {
     let tapgesture = UITapGestureRecognizer(target: self, action: #selector(tappedOnLabel(_ :)))
     tapgesture.numberOfTapsRequired = 1
     label.addGestureRecognizer(tapgesture)
-
+    
   }
   
   
   @objc func tappedOnLabel(_ gesture: UITapGestureRecognizer) {
-    guard let text = label.text else { return }
     if let url = gesture.didTapAttributedTextInLabel(label: label, inRange: urlString) {
       print(url)
       if let openUrl = URL(string: url) {
-          UIApplication.shared.open(openUrl)
+        UIApplication.shared.open(openUrl)
       }
     }
   }
-
+  
   func teste () {
     guard let input = label.text else {return}
     let detector = try! NSDataDetector(types: NSTextCheckingResult.CheckingType.link.rawValue)
     let matches = detector.matches(in: input, options: [], range: NSRange(location: 0, length: input.utf16.count))
-
+    
     for match in matches {
-        guard let range = Range(match.range, in: input) else { continue }
-        let url = String(input[range])
-        print(url)
-        urlString[url] = match.range
+      guard let range = Range(match.range, in: input) else { continue }
+      let url = String(input[range])
+      print(url)
+      urlString[url] = match.range
+      print(urlString)
     }
   }
 }
 
 extension UITapGestureRecognizer {
-    func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: [String : NSRange] ) -> String? {
-
-        // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
-        let layoutManager = NSLayoutManager()
-        let textContainer = NSTextContainer(size: CGSize.zero)
-        let textStorage = NSTextStorage(attributedString: label.attributedText!)
-
-        // Configure layoutManager and textStorage
-        layoutManager.addTextContainer(textContainer)
-        textStorage.addLayoutManager(layoutManager)
-
-        // Configure textContainer
-        textContainer.lineFragmentPadding = 0.0
-        textContainer.lineBreakMode = label.lineBreakMode
-        textContainer.maximumNumberOfLines = label.numberOfLines
-        let labelSize = label.bounds.size
-        textContainer.size = labelSize
-
-        // Find the tapped character location and compare it to the specified range
-        let locationOfTouchInLabel = self.location(in: label)
-        let textBoundingBox = layoutManager.usedRect(for: textContainer)
-        let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
-        let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
-        let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
-      print(indexOfCharacter)
-      
-      for (key,value) in targetRange {
-        if NSLocationInRange(indexOfCharacter, value) {
-          return key
-        }
+  func didTapAttributedTextInLabel(label: UILabel, inRange targetRange: [String : NSRange] ) -> String? {
+    
+    // Create instances of NSLayoutManager, NSTextContainer and NSTextStorage
+    let layoutManager = NSLayoutManager()
+    let textContainer = NSTextContainer(size: CGSize.zero)
+    let textStorage = NSTextStorage(attributedString: label.attributedText!)
+    
+    // Configure layoutManager and textStorage
+    layoutManager.addTextContainer(textContainer)
+    textStorage.addLayoutManager(layoutManager)
+    
+    // Configure textContainer
+    textContainer.lineFragmentPadding = 0.0
+    textContainer.lineBreakMode = label.lineBreakMode
+    textContainer.maximumNumberOfLines = label.numberOfLines
+    let labelSize = label.bounds.size
+    textContainer.size = labelSize
+    
+    // Find the tapped character location and compare it to the specified range
+    let locationOfTouchInLabel = self.location(in: label)
+    let textBoundingBox = layoutManager.usedRect(for: textContainer)
+    let textContainerOffset = CGPoint(x: (labelSize.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x, y: (labelSize.height - textBoundingBox.size.height) * 0.5 - textBoundingBox.origin.y)
+    let locationOfTouchInTextContainer = CGPoint(x: locationOfTouchInLabel.x - textContainerOffset.x, y: locationOfTouchInLabel.y - textContainerOffset.y)
+    let indexOfCharacter = layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
+    print(indexOfCharacter)
+    
+    for (key,value) in targetRange {
+      if NSLocationInRange(indexOfCharacter, value) {
+        return key
       }
-      return nil
     }
+    return nil
+  }
 }
